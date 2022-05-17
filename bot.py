8450a91e8b6bffafbe4752d1fcdf332a2cc518b5
soumadip <soumadip377@gmail.com>
2022-05-17 16:50:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -35,7 +35,7 @@ except:
     exit()
 
 
-@datgbot.on(events.NewMessage(pattern="/start"))
+@datgbot.on(events.NewMessage(pattern="/run"))
 async def _(event):
     ok = await datgbot(GetFullUserRequest(event.sender_id))
     await event.reply(f"Hi `{ok.user.first_name}`!\n\nI am a channel auto-post bot!! Read /help to know more!\n\nI can be used in only two channels (one user) at a time. Kindly deploy your own bot.\n\n[More bots](https://t.me/its_xditya)..", buttons=[Button.url("Repo", url="https://github.com/xditya/ChannelAutoForwarder"), Button.url("Dev", url="https://t.me/its_xditya")], link_preview=False)
@@ -43,7 +43,7 @@ async def _(event):
 
 @datgbot.on(events.NewMessage(pattern="/help"))
 async def helpp(event):
-    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from [here](https://github.com/xditya/ChannelAutoForwarder).\n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @xditya_Bot :)")
+    await event.reply("**Help**\n\nThis bot will send all new posts in one channel to the other channel. (without forwarded tag)!\nIt can be used only in two channels at a time, so kindly deploy your own bot from [here](https://github.com/soymadip).\n\nAdd me to both the channels and make me an admin in both, and all new messages would be autoposted on the linked channel!!\n\nLiked the bot? Drop a ♥ to @xditya_Bot :)")
 
 @datgbot.on(events.NewMessage(incoming=True, chats=frm)) 
 async def _(event): 
@@ -52,8 +52,7 @@ async def _(event):
             if event.poll:
                 return
             if event.photo:
-                photo = event.media.photo
-                await datgbot.send_file(tochnl, photo, caption = event.text, link_preview = False)
+                print("skipped pic.")
             elif event.media:
                 try:
                     if event.media.webpage:
@@ -64,7 +63,7 @@ async def _(event):
                     await datgbot.send_file(tochnl, media, caption = event.text, link_preview = False)
                     return
             else:
-                await datgbot.send_message(tochnl, event.text, link_preview = False)
+                print("skipped text.")
         except:
             print("TO_CHANNEL ID is wrong or I can't send messages there (make me admin).")
 
